a992cfe5718510515edb8c7e7c2139bbaeb7aa8b
Seth Karim Luis Martínez <ing.seth.luis@gmail.com>
2018-07-08 08:44:54 -0500

    Creado método post para guardar social medias

--- src/main/java/com/blackdeath/profesoresplatzi/controller/SocialMediaController.java
+++ src/main/java/com/blackdeath/profesoresplatzi/controller/SocialMediaController.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.blackdeath.profesoresplatzi.model.SocialMedia;
 import com.blackdeath.profesoresplatzi.service.SocialMediaService;
@@ -37,4 +41,30 @@ public class SocialMediaController {
 		return new ResponseEntity<List<SocialMedia>>(socialMedias, HttpStatus.OK);
 	}
 
+	// POST
+	@RequestMapping(value = "/socialMedias", method = RequestMethod.POST, headers = "Accept=application/json")
+	public ResponseEntity<?> createSocialMedia(@RequestBody SocialMedia socialMedia,
+			UriComponentsBuilder uriComponentsBuilder) {
+		if (socialMedia.getName() == null || socialMedia.getName().isEmpty()) {
+			// return new ResponseEntity(HttpStatus.NO_CONTENT);
+			return ResponseEntity.noContent().build();
+		}
+
+		if (socialMediaService.findByName(socialMedia.getName()) != null) {
+			// return new ResponseEntity(HttpStatus.NO_CONTENT);
+			return ResponseEntity.noContent().build();
+		}
+
+		socialMediaService.saveSocialMedia(socialMedia);
+
+		SocialMedia socialMedia2 = socialMediaService.findByName(socialMedia.getName());
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(uriComponentsBuilder.path("/v1/socialMedias/{id}")
+				.buildAndExpand(socialMedia2.getIdSocialMedia()).toUri());
+
+		// return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+		return ResponseEntity.created(uriComponentsBuilder.path("/v1/socialMedias/{id}")
+				.buildAndExpand(socialMedia2.getIdSocialMedia()).toUri()).build();
+	}
 }
