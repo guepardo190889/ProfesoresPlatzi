702bda28bdc697f0d146888c46be2775a90c3f4f
Seth Karim Luis Martinez <guepardo190889@gmail.com>
2018-07-27 07:06:02 -0500

    cambios

--- src/main/java/com/blackdeath/profesoresplatzi/controller/SocialMediaController.java
+++ src/main/java/com/blackdeath/profesoresplatzi/controller/SocialMediaController.java
@@ -16,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.blackdeath.profesoresplatzi.model.SocialMedia;
 import com.blackdeath.profesoresplatzi.service.SocialMediaService;
+import com.blackdeath.profesoresplatzi.util.CustomErrorType;
 
 /**
  * @author Seth Luis
@@ -44,8 +45,8 @@ public class SocialMediaController {
 	// GET
 	@RequestMapping(value = "/socialMedias/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
 	public ResponseEntity<SocialMedia> getSocialMediaById(@PathVariable("id") Long idSocialMedia) {
-		if (idSocialMedia <= 0) {
-			return ResponseEntity.noContent().build();
+		if (idSocialMedia == null || idSocialMedia <= 0) {
+			return new ResponseEntity(new CustomErrorType("idSocialMedia es requerido"), HttpStatus.CONFLICT);
 		}
 
 		SocialMedia socialMedia = socialMediaService.findById(idSocialMedia);
@@ -62,7 +63,7 @@ public class SocialMediaController {
 	public ResponseEntity<?> createSocialMedia(@RequestBody SocialMedia socialMedia,
 			UriComponentsBuilder uriComponentsBuilder) {
 		if (socialMedia.getName() == null || socialMedia.getName().isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity(new CustomErrorType("name es requerido"), HttpStatus.CONFLICT);
 		}
 
 		if (socialMediaService.findByName(socialMedia.getName()) != null) {
@@ -79,4 +80,45 @@ public class SocialMediaController {
 
 		return new ResponseEntity<String>(headers, HttpStatus.CREATED);
 	}
+
+	// UPDATE
+	@RequestMapping(value = "/socialMedias/{id}", method = RequestMethod.PATCH)
+	public ResponseEntity<SocialMedia> updateSocialMedia(@PathVariable("id") Long idSocialMedia,
+			@RequestBody SocialMedia socialMedia) {
+		if (idSocialMedia == null || idSocialMedia <= 0) {
+			return new ResponseEntity(new CustomErrorType("idSocialMedia es requerido"), HttpStatus.CONFLICT);
+		}
+
+		SocialMedia currentSocialMedia = socialMediaService.findById(idSocialMedia);
+
+		if (currentSocialMedia == null) {
+			return ResponseEntity.noContent().build();
+		}
+
+		currentSocialMedia.setName(socialMedia.getName());
+		currentSocialMedia.setIcon(socialMedia.getIcon());
+
+		socialMediaService.updateSocialMedia(currentSocialMedia);
+
+		return new ResponseEntity<SocialMedia>(currentSocialMedia, HttpStatus.OK);
+	}
+
+	// DELETE
+	@RequestMapping(value = "/socialMedias/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteSocialMedia(@PathVariable("id") Long idSocialMedia) {
+		if (idSocialMedia == null || idSocialMedia <= 0) {
+			return new ResponseEntity(new CustomErrorType("idSocialMedia es requerido"), HttpStatus.CONFLICT);
+		}
+
+		SocialMedia socialMedia = socialMediaService.findById(idSocialMedia);
+
+		if (socialMedia == null) {
+			return ResponseEntity.noContent().build();
+		}
+
+		socialMediaService.deleteSocialMediaById(idSocialMedia);
+
+		return new ResponseEntity<SocialMedia>(HttpStatus.OK);
+	}
+
 }
